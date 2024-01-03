01b304ef4221ee63fbde5613dbab6496aad1c76c
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-01-03 12:55:25 +0600

    Average of five integers

---
+++ chapter-3/Exercise_2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exercise_2{
+
+	public static void main (String args[]){
+	
+	Scanner input = new Scanner(System.in);
+	int a,b,c,d,e;
+	
+	a = input.nextInt();
+	b = input.nextInt();
+	c = input.nextInt();
+	d = input.nextInt();
+	e = input.nextInt();
+	
+	System.out.println((a+b+c+d+e)/5.0);
+	
+		
+	}
+};
