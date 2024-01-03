427aa376c7d83d63e6651584de3c29e454e24062
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-01-03 13:06:58 +0600

    Calculating body mass index(BMI)

---
+++ chapter-3/Exercise_3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercise_3{
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		
+		int w;
+		double h;
+		w = input.nextInt();
+		h = input.nextDouble();
+		
+		System.out.println(w/ (h*h));
+		
+	}
+};
