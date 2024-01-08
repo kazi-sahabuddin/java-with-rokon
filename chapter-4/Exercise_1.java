b27a9f79edd3d69f427872b45786eec27a6ab681
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-01-08 18:04:18 +0600

    pattern print 01

---
+++ chapter-4/Exercise_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercise_1{
+	public static void main(String args[]){
+		for(int i =0; i<10; i++){
+			int ch =65+i;
+			for(int j=0; j<=i; j++){
+				System.out.print((char)ch+" ");			
+			}
+			System.out.println();
+		
+		}
+	
+	}
+}
