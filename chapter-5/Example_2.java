f5d1ca7d508998b6e144e83ac2346a9871cfd135
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-01-10 15:46:50 +0600

    Frequency of characters in a string

---
+++ chapter-5/Example_2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Example_2{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter characters: ");
+		String inputValue = scan.nextLine();
+		inputValue = inputValue.toLowerCase();
+		char[] letters = inputValue.toCharArray();
+		
+		int[] counts = new int[26];
+		
+		for(int i = 0; i < letters.length; i++){
+		
+			if(Character.isLetter(letters[i])){
+				int index = letters[i] - 'a';
+				counts[index]++;
+			}
+			
+		}
+		
+		System.out.println("letter\tCount");
+		for(int i = 0; i < counts.length; i++){
+			System.out.println( (char)(i+'a') +"\t"+counts[i]);
+		}
+		
+	}
+}
