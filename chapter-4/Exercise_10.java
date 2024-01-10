55836a6cc9ada3e89cbdfbda5972560722f26eed
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-01-10 10:13:46 +0600

    Priramid print

---
+++ chapter-4/Exercise_10.java
@@ -0,0 +1,15 @@
+public class Exercise_10{
+	public static void main(String args[]){
+		for(int i = 1; i <= 9; i++){
+			for(int j = 1; j <= 9-i; j++){
+				System.out.print(" ");
+			}
+			
+			for(int j = 1; j <= i; j++){
+				System.out.print(i+" ");
+			}
+			
+			System.out.println();
+		}
+	}
+}
