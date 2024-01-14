a503ed4bc08716f942d4772a6e131daabfc963a6
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-01-14 07:02:54 +0600

    Flight Class

---
+++ chapter-6/Flight.java
@@ -0,0 +1,43 @@
+package com.kazi.chapter6;
+
+public class Flight{
+	private static final int TOTAL_SETTING_CAPACITY = 150;
+	private int totalPassengers;
+	private int totalCheckedBags;
+	
+	public void addOnePassenger(int bags, int carryOns){
+	
+		int totalBags = bags;
+		if(carryOns > 2){
+			totalBags = bags + carryOns - 2;
+		}
+		
+		addOnePassenger(totalBags);	
+	}
+	
+	public void addOnePassenger(int bags){
+		if(isSeatAvailable()){
+			addOnePassenger();
+			totalCheckedBags += bags;
+		}
+	}
+	
+	private boolean isSeatAvailable(){
+		return totalPassengers < TOTAL_SETTING_CAPACITY;
+	}
+	
+	public void addOnePassenger(){
+	
+		if(isSeatAvailable()){
+			totalPassengers += 1;
+		}		
+	}
+	
+	public int getTotalCheckedBags(){
+		return totalCheckedBags;
+	}
+	
+	public int getTotalPassengers(){
+		return totalPassengers;
+	}
+}
