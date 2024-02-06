e271b8c981b22217d34040d34e34e5888e6e9bf2
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-02-06 16:38:35 +0600

    Multiple try catch and or catch

--- chapter-8/MultipleCatchDemo.java
+++ chapter-8/MultipleCatchDemo.java
@@ -14,11 +14,17 @@ public class MultipleCatchDemo{
 		
 		try{
 			int number[] = new int[10];
-			number[15] = 30/1;
+			number[15] = 30/0;
 			} catch(ArithmeticException | ArrayIndexOutOfBoundsException e){
 			if( e instanceof ArithmeticException){
+				
+				System.out.println(e.getMessage());
 				System.out.println("You cannot divide by zero");
-				e.printStackTrace();
+				
+				System.out.println(e.toString());
+				
+				//e.printStackTrace();
+				
 			} else if( e instanceof ArrayIndexOutOfBoundsException){
 				System.out.println("The index you tried to access dose not exist");
 				e.printStackTrace();
