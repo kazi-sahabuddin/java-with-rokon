d1b49c3a5de58ee29033755be88d774a31bb9263
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-02-08 20:51:45 +0600

    Java Path Demo

---
+++ chapter-10/JavaPathDemo.java
@@ -0,0 +1,9 @@
+import java.nio.file.Path;
+
+public class JavaPathDemo{
+	public static void main(String[] args){
+		Path path = Path.of("/home/sahabuddin/Desktop/javaWithRokon/chapter-10/hello.txt");
+		
+		System.out.println(path);
+	}
+}
