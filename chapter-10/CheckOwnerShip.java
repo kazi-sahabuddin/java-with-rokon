c92d1637252844effad3c56741b5caed902d87fa
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-02-09 11:26:18 +0600

    Check ownershio of a file

---
+++ chapter-10/CheckOwnerShip.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.UserPrincipal;
+
+public class CheckOwnerShip{
+	public static void main(String[] args) throws IOException{
+		Path path = Path.of("/home/sahabuddin/Desktop/javaWithRokon/chapter-10/hello.txt");
+		UserPrincipal owner = Files.getOwner(path);
+		System.out.println("owner = "+ owner.getName());
+	}
+}
