3c38e15a6d081fbf4df67e4feaccda71195efb2e
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-02-09 11:38:46 +0600

    Check ownershio of a file

--- chapter-10/CheckOwnerShip.java
+++ chapter-10/CheckOwnerShip.java
@@ -4,9 +4,20 @@ import java.nio.file.Path;
 import java.nio.file.attribute.UserPrincipal;
 
 public class CheckOwnerShip{
-	public static void main(String[] args) throws IOException{
-		Path path = Path.of("hello.txt");
+	public static void main(String[] args) throws IOException{	
+		Path path = Path.of("helloo.txt");
 		UserPrincipal owner = Files.getOwner(path);
 		System.out.println("owner = "+ owner.getName());
 	}
+	
+	/*public static void main(String[] args) {
+	try{
+		Path path = Path.of("helloo.txt");
+		UserPrincipal owner = Files.getOwner(path);
+		System.out.println("owner = "+ owner.getName());
+	} catch(IOException e){
+		System.out.println(e);
+	}
+		
+	}*/
 }
