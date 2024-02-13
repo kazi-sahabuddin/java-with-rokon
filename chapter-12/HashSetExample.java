94379af26bb2919473f3057b01a735cfa393ab29
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-02-13 11:22:49 +0600

    Example of hashSet

--- chapter-12/HashSetExample.java
+++ chapter-12/HashSetExample.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class HasSetExample{
+public class HashSetExample{
 	public static void main(String[] args){
 		Set<String> cities = new HashSet<>();
 		
