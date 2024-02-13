ecfacebac733be54c8e77e00cb48191180ff7311
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-02-13 15:10:33 +0600

    Example of Queue

---
+++ chapter-12/QueueExample.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample{
+	public static void main(String[] args){
+		Queue<Integer> queue = new LinkedList<>();
+		for(int i = 10; i >=0; i--){
+			queue.add(i);
+		}
+		while(!queue.isEmpty()){
+			Integer value = queue.poll();
+			System.out.print(value + ", ");
+		}
+		System.out.println();
+	}
+}
