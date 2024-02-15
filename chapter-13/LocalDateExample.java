3d97b4fab0a6446e644edbe24d33ff7aab67fd88
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-02-15 10:52:56 +0600

    Example of Local Date

---
+++ chapter-13/LocalDateExample.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class LocalDateExample{
+	public static void main(String[] args){
+		LocalDate today = LocalDate.now();
+		System.out.println("today = "+today);
+	}
+}
