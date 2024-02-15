ba98db97f07ef01322b576d956be58e0a03b25d5
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-02-15 10:25:20 +0600

    Example of calendar

---
+++ chapter-13/CalenderExample.java
@@ -0,0 +1,22 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class CalenderExample{
+	public static void main(String[] args){
+		Calendar calendar = new GregorianCalendar(2023, Calendar.FEBRUARY, 28,13,24,56);
+		
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH);
+		int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+		int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+		int weekOfYear = calendar.get(Calendar.WEEK_OF_YEAR);
+		int weekOfMonth = calendar.get(Calendar.WEEK_OF_MONTH);
+		
+		System.out.println("Year \t\t: " + year);
+		System.out.println("month \t\t: " + month);
+		System.out.println("dayOfMonth \t: " + dayOfMonth);
+		System.out.println("dayOfWeek \t: " + dayOfWeek);
+		System.out.println("weekOfYear \t: " + weekOfYear);
+		System.out.println("weekOfMonth \t: " + weekOfMonth);
+	}
+}
